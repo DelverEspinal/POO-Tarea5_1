d5dbaca083c15d7ae20a4d41ed42c993fe1d059b
DelverEspinal <delvervarela66@gmail.com>
2025-08-19 22:46:48 -0600

    Directorio
    Proyecto en el que se replica el ejercicio de la video conferencia

---
+++ Directorio/Program.cs
@@ -0,0 +1,82 @@
+﻿using Directorio.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directorio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            //Declaracion de objeto list
+            List<string> frutas = new List<string>();
+
+            frutas.Add("Manzana");
+            frutas.Add("Pera");
+
+            //Console.WriteLine(frutas[0]);
+            //Console.WriteLine(frutas[1]);
+
+            //Estructura de repeticion for each para recorrer una lista
+            foreach (string fruta in frutas)
+            {
+                Console.WriteLine(fruta);
+            }
+            */
+
+            List<Persona> personas = new List<Persona>();
+
+            Persona per1 = new Persona("Gabriel", "Garcia Marquez");
+            Persona per2 = new Persona("Jose", "Saramargo");
+
+            personas.Add(per1);
+            personas.Add(per2);
+
+            foreach (Persona per in personas)
+            {
+                Console.WriteLine(per.getNombreCompleto());
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+
+        
+             
+            //Estos es lo que habiamos hecho anteriormente
+
+            static void ejemploClassPersona()
+        {
+
+            Persona persona = new Persona();
+            Marcador marcador = new Marcador();
+
+
+            string nombreCompleto = string.Empty;
+
+            persona.setNombre("Pedro Antonio");
+            persona.setApellidos("Perez Perez");
+
+            nombreCompleto = persona.getNombreCompleto();
+            Console.WriteLine("Persona 1" + " " + nombreCompleto + " " + "Salario: " + marcador.getSalario(44 * 4));
+
+            nombreCompleto = persona.getNombreCompleto("Jose Cecilio ", "Del Valle");
+
+            Console.WriteLine("Persona 2" + " " + nombreCompleto + " " + "Salario: " + marcador.getSalario(35 * 4));
+
+                Console.ReadLine();
+
+
+
+        }
+            
+
+    
+    }
+}
